2000fd2cecf6cf6259d3841879d5a44a8f5d004f
thanh hai <hai.thanh.dinh@mgm-tp.com>
2022-10-14 06:52:15 +0700

    fix: sitemap

--- config/site.js
+++ config/site.js
@@ -24,8 +24,13 @@ module.exports = {
   url: 'https://blog.maximeheckel.com', // Site domain without trailing slash
   siteUrl: 'https://blog.maximeheckel.com/', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
+  // logo: 'src/static/logo/logo.png',
+  image:
+    'https://res.cloudinary.com/anhchangtoanhoc97/image/upload/v1665705108/icon_wotfhe.png',
+  // favicon: 'static/favicon.png', // Manifest favicon generation
   shortName: 'Dinh', // Shortname for manifest, must be shorter than 12 characters
   author: 'Thanh Hai', // Author for schemaORGJSONLD
   themeColor: '#000000',
   backgroundColor: '#ffffff',
+  twitter: '@MaximeHeckel', // Twitter Username
 };
